d63458ec9b398a63528fee7cd0ed58729f1e6edd
chiko911 <71180624+chiko911@users.noreply.github.com>
2023-04-11 23:39:04 +0200

    Update sui.sh

--- sui.sh
+++ sui.sh
@@ -1,68 +1,58 @@
 #!/bin/bash
 
-exists()
-{
-  command -v "$1" >/dev/null 2>&1
+function logo {
+  bash <(curl -s https://raw.githubusercontent.com/chiko911/useful-scripts/introduce.sh)
 }
-if exists curl; then
-	echo ''
-else
-  sudo apt update && sudo apt install curl -y < "/dev/null"
-fi
-bash_profile=$HOME/.bash_profile
-if [ -f "$bash_profile" ]; then
-    . $HOME/.bash_profile
-fi
-sleep 1 && curl -s https://raw.githubusercontent.com/chiko911/useful-scripts/main/introduce.sh | bash && sleep 2
 
-echo -e '\n\e[42mInstalling required software...\e[0m\n' && sleep 1
-apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends tzdata git ca-certificates curl build-essential libssl-dev pkg-config libclang-dev cmake jq
-echo -e '\n\e[42mInstall Rust\e[0m\n' && sleep 1
-sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
+function line {
+  echo "-----------------------------------------------------------------------------"
+}
 
-rm -rf /var/sui/db /var/sui/genesis.blob $HOME/sui
-mkdir -p /var/sui/db
-cd $HOME
-git clone https://github.com/MystenLabs/sui.git
-cd sui
-git checkout 99b4e7ca83b6d1abe312f5afc04840dce331238b
-cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
-sed -i.bak "s/db-path:.*/db-path: \"\/var\/sui\/suidb\"/ ; s/genesis-file-location:.*/genesis-file-location: \"\/var\/sui\/genesis.blob\"/" /var/sui/fullnode.yaml
-wget -O /var/sui/genesis.blob https://github.com/SuiExternal/sui-external/raw/main/genesis.blob
-cargo build --release -p sui-node
-mv ~/sui/target/release/sui-node /usr/local/bin/ || exit
+function colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
 
-echo "[Unit]
-Description=Sui Node
-After=network.target
+function main_tools {
+  bash <(curl -s https://raw.githubusercontent.com/chiko911/useful-scripts/tools.sh)
+}
 
-[Service]
-User=$USER
-Type=simple
-ExecStart=/usr/local/bin/sui-node --config-path /var/sui/fullnode.yaml
-Restart=on-failure
-LimitNOFILE=65535
+function docker {
+  bash <(curl -s https://raw.githubusercontent.com/chiko911/useful-scripts/docker.sh)
+}
 
-[Install]
-WantedBy=multi-user.target" > $HOME/suid.service
+function prepare {
+  mkdir -p $HOME/sui
+  cd $HOME/sui
+  wget -O $HOME/sui/fullnode-template.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+  wget -O $HOME/sui/genesis.blob  https://github.com/MystenLabs/sui-genesis/raw/main/testnet/genesis.blob
+  IMAGE="mysten/sui-node:92b4c0671b746a14cac658e2283c0ce01c6a98f3"
+  wget -O $HOME/sui/docker-compose.yaml https://raw.githubusercontent.com/MystenLabs/sui/main/docker/fullnode/docker-compose.yaml
+  sed -i.bak "s|image:.*|image: $IMAGE|" $HOME/sui/docker-compose.yaml
+}
 
-mv $HOME/suid.service /etc/systemd/system/
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
-EOF
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-sudo systemctl enable suid
-sudo systemctl restart suid
+function run_docker {
+  docker-compose -f ${HOME}/sui/docker-compose.yaml pull
+  docker-compose -f ${HOME}/sui/docker-compose.yaml up -d
+}
 
-
-echo "==================================================="
-echo -e '\n\e[42mCheck Sui status\e[0m\n' && sleep 1
-if [[ `service suid status | grep active` =~ "running" ]]; then
-  echo -e "Your Sui Node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice suid status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
-else
-  echo -e "Your Sui Node \e[31mwas not installed correctly\e[39m, please reinstall."
-fi
+colors
+line
+logo
+line
+echo "installing tools...."
+line
+main_tools
+docker
+line
+echo "prepare directory and docker files"
+line
+prepare
+line
+echo "starting docker-compose"
+line
+run_docker
+line
+echo "installation complete, check logs by command:"
+echo "docker-compose -f $HOME/sui/docker-compose.yaml logs -f --tail=100"
